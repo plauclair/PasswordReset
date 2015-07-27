41f3da40b016845cbde53411c08e598e9f92da4c
Pierre-Luc Auclair <p-l.auclair@velourbain.qc.ca>
2015-07-27 16:05:13 -0400

    Some changes
    - Change the way getScripts() works
    - readme.md
    - Add dummy template file

---
+++ password-reset.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Password Reser module template file
+ *
+ * Copy this file to your templates folder.
+ */
+
+// You just need to add the controller on the template page...
+$content = $modules->get('PasswordReset')->controller();
+// or echo it based on your needs echo $modules->get('Pass...
+
+// ... and load the scripts
+// echo $modules->get('PasswordReset')->getScripts();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Password Reset
+
+A frontend password reset module for ProcessWire.
+ 
+## Requirements
+- PHP >= 5.3.0
+- ProcessWire >= 2.6
+
+## Usage
+Copy the password-reset.php file to your site's templates folder.
+
+## Public methods
+
+### getScripts()
+Returns
